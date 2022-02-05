b49c947f2cff815482a832ac948430a12adf8035
Christopher Le <locvicvn1234@gmail.com>
2022-02-04 19:31:04 -0600

    Time: 52 ms (69.80%), Space: 14.9 MB (83.25%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,45 +1,5 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        """
-        nums = [1,3,5,6], target = 2
-        nums = [1,2,3,5,6]
-        1
-        
-        nums = [1,3,5,6], target = 7
-        nums = [1,3,5,6,7]
-        -> 4
-        
-        nums = [1,3,5,6], target = 4
-        [1,3,4,5,6]
-        return pivot[index of 3] + 1
-        
-        Input: nums = [1,3,5,6], target = 7
-        
-        
-        nums = [1,3,5,6], target = 5
-        mid = len(nums) / 2
-    
-        nums[mid] # 5 == 5 return mid
-        
-        
-        nums = [1,2,3,5,6], target = 2
-        mid = 2
-        nums[mid] = 5 > 2
-        0 - 2
-        mid = 2 // 2 = 1
-        3 > 2
-        0 1 // 2= 0
-        
-        nums[0] = 1 < 2
-        ret
-        
-        nums = [1,3,5,6], target = 2
-        
-        
-        """
-
-        
+    def searchInsert(self, nums: List[int], target: int) -> int:   
         left, right = 0, len(nums)-1
         while left <= right:
             mid = (left+right+1) // 2
