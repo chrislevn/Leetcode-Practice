dce0f902d2e96b0d8d51e84658696466f82e3169
Christopher Le <locvicvn1234@gmail.com>
2022-02-04 19:27:22 -0600

    Time: 78 ms (30.35%), Space: 14.8 MB (83.25%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -15,13 +15,54 @@ class Solution:
         return pivot[index of 3] + 1
         
         Input: nums = [1,3,5,6], target = 7
-
+        
+        
+        nums = [1,3,5,6], target = 5
+        mid = len(nums) / 2
+    
+        nums[mid] # 5 == 5 return mid
+        
+        
+        nums = [1,2,3,5,6], target = 2
+        mid = 2
+        nums[mid] = 5 > 2
+        0 - 2
+        mid = 2 // 2 = 1
+        3 > 2
+        0 1 // 2= 0
+        
+        nums[0] = 1 < 2
+        ret
+        
+        nums = [1,3,5,6], target = 2
+        
         
         """
+
         
-        nums.append(target)
-        nums = sorted(nums)
+        left, right = 0, len(nums)-1
         
-        return nums.index(target)
+        if nums[0] == target: 
+            return 0
+        if nums[-1] < target: 
+            return right + 1
+        if nums[0] > target: 
+            return 0
+    
+        while (right - left) > 1:
+            mid = (left+right+1) // 2
+            if nums[mid] == target: 
+                return mid
+            elif nums[mid] < target: 
+                left = mid
+            else: 
+                right = mid
+                
+        return left + 1
+        
+        
+        
+        
+        
             
         
