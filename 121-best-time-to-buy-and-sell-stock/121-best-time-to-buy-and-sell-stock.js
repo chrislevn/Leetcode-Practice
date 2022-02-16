d5d56077a739de68acbedb7fac943a8e6b0ec6e3
Christopher Le <locvicvn1234@gmail.com>
2022-02-15 20:24:28 -0600

    Time: 91 ms (86.52%), Space: 51.7 MB (35.51%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (prices.length == 1) { 
+        return 0;
+    } 
+    var index = 0; 
+    var result = 0; 
+    
+    for (var i = 1; i < prices.length; i++) {
+        if (prices[i] - prices[index] > 0) {
+            result = Math.max(result, prices[i] - prices[index])
+        } else {
+            index = i;
+        }
+    }
+    
+    return result;
+    
+};
