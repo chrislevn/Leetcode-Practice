df3a6499f309ea87a8c3f57b3f2c1064875b5de9
Christopher Le <locvicvn1234@gmail.com>
2022-03-01 14:41:42 -0600

    Time: 36 ms (78.35%), Space: 13.9 MB (50.12%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,3 +1,12 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        return sum([i//7 + i%7 + 1 for i in range(n)])
+        divide_part = n // 7 
+        remain = n % 7 
+        
+        result = 0
+        for i in range(1, divide_part+1): 
+            result += sum(range(i,i+7,1))
+        
+        result += sum(range((divide_part + 1), (divide_part + 1)+remain, 1))
+        
+        return result
