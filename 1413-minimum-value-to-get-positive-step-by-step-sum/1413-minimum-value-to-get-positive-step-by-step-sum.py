a04d819593e00b2877214bbdf5f90b05a1f308ba
Christopher Le <locvicvn1234@gmail.com>
2022-04-06 15:26:30 -0500

    Time: 48 ms (51.30%), Space: 13.9 MB (59.96%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,25 +1,12 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        startValue = 1
-        while not self.checkNum(startValue, nums): 
-            startValue += 1
-        return startValue
+        sumValue = 0
+        minSum = 0
         
-    def checkNum(self, startValue, nums: List[int]) -> bool:
-        """Calculate value after iterating through the nums value. 
-        
-        Args: 
-            startValue (int): starting value. 
-            nums (list): list of integers. 
+        for i in nums: 
+            sumValue += i 
+            minSum = min(minSum, sumValue)
             
-        Returns: 
-            Whether the result value meets the requirement.
-
-        """
+        return 1 - minSum
         
-        for i in range(len(nums)): 
-            startValue += nums[i]
-            if startValue < 1: 
-                return False
-        return True
         
