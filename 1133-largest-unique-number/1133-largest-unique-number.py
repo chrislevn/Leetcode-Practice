dfe06924bc3c8d74435c26ebc79c78d428291303
Christopher Le <locvicvn1234@gmail.com>
2022-04-29 15:13:33 -0500

    Time: 54 ms (70.92%), Space: 14.1 MB (39.84%) - LeetHub

---
+++ 1133-largest-unique-number/1133-largest-unique-number.py
@@ -0,0 +1,18 @@
+from collections import OrderedDict 
+
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        hashTable = OrderedDict()
+        res = -1
+        
+        for num in nums: 
+            if num not in hashTable: 
+                hashTable[num] = 1
+            else: 
+                hashTable[num] += 1
+                                
+        for item, value in hashTable.items(): 
+            if value == 1: 
+                res = max(res, item)
+                
+        return res
