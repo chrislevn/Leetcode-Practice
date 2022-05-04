42dfdd9be115b0739c9923cd85b6b05d02376b7a
Christopher Le <locvicvn1234@gmail.com>
2022-05-03 21:17:40 -0500

    Time: 143 ms (30.24%), Space: 14.2 MB (45.49%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -1,20 +1,20 @@
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        result = []
-        for num in arr1: 
-            if self.binarySearch(0, len(arr2), arr2, num) and self.binarySearch(0, len(arr3), arr3, num): 
-                result.append(num)
-        return result
-    
-        
-    def binarySearch(self, left, right, arr, value):
-        while left < right:
-            mid = (left + right) // 2
-            if arr[mid] == value: 
-                return True
-            elif value > arr[mid]: 
-                left = mid+1
-            else: 
-                right = mid
-                
-        return False
+        ans = []
+        # prepare three pointers to iterate through three arrays
+        # p1, p2, and p3 point to the beginning of arr1, arr2, and arr3 accordingly
+        p1 = p2 = p3 = 0
+        while p1 < len(arr1) and p2 < len(arr2) and p3 < len(arr3):
+            if arr1[p1] == arr2[p2] == arr3[p3]:
+                ans.append(arr1[p1])
+                p1 += 1
+                p2 += 1
+                p3 += 1
+            else:
+                if arr1[p1] < arr2[p2]:
+                    p1 += 1
+                elif arr2[p2] < arr3[p3]:
+                    p2 += 1
+                else:
+                    p3 += 1
+        return ans
