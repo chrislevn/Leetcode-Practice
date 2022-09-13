4d2506568903f6d5c71af8030385de65d6adead4
Christopher Le <locvicvn1234@gmail.com>
2022-09-12 19:26:22 -0500

    Time: 1381 ms (7.16%), Space: 27.9 MB (81.89%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
@@ -1,11 +1,12 @@
 class Solution:
     def addRungs(self, rungs: List[int], dist: int) -> int:
-        curr, res = 0, 0
+        pre, res = 0, 0
         length = len(rungs)
         
         for i in range(length): 
-            if (rungs[i] - curr) > dist: 
-                res += (rungs[i] - curr - 1) // dist
-            curr = rungs[i] 
+            curr = rungs[i]
+            if (curr - pre) > dist: 
+                res += (curr - pre - 1) // dist
+            pre = curr
         return res
             
