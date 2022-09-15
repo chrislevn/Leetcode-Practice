eb47fc466a17054176540fd83c4b946a92e68451
Christopher Le <locvicvn1234@gmail.com>
2022-09-14 19:22:47 -0500

    Time: 171 ms (25.70%), Space: 14.2 MB (5.35%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
@@ -1,10 +1,14 @@
 class Solution:
     def mostFrequent(self, nums: List[int], key: int) -> int:
         freqDict = {}
+        temp, res = -1, 0
         for i in range(len(nums)-1): 
             if nums[i] == key: 
                 if nums[i+1] not in freqDict: 
                     freqDict[nums[i+1]] = 1
                 else: 
                     freqDict[nums[i+1]] += 1
-        return max(freqDict, key=freqDict.get)
+                if freqDict[nums[i+1]] > temp: 
+                    temp = freqDict[nums[i+1]]
+                    res = nums[i+1]
+        return res
