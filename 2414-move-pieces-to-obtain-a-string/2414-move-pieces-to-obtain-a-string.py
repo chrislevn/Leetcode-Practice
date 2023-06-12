ac269b10ca79632bffdf1e3d59d48a4ace229919
Christopher Le <locvicvn1234@gmail.com>
2023-06-12 17:07:04 -0500

    Time: 294 ms (47.39%), Space: 17.5 MB (34.35%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
@@ -0,0 +1,22 @@
+# import pandas as pd 
+
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        if start.replace('_', '') != target.replace('_', ''):
+            return False
+        i = j = 0
+        n = len(start)
+        while i < n and j < n:
+            while i < n and start[i] == '_':
+                i += 1
+            while j < n and target[j] == '_':
+                j += 1
+            if i < n and j < n and (start[i] == 'L' and i < j or start[i] == 'R' and i > j):
+                return False
+            i += 1
+            j += 1
+        return True
+
+
+        
+
